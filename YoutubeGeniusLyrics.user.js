2ec450f89038d73b7cb1e7d1be4da8c73bf4cab0
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2022-12-20 13:46:47 +0900

    variable checking is added

--- YoutubeGeniusLyrics.user.js
+++ YoutubeGeniusLyrics.user.js
@@ -1902,6 +1902,7 @@ if (document.location.hostname.startsWith('music')) {
       document.addEventListener('yt-navigate-finish', delayedMain, false)
     }
 
+  genius = null
   if (isInIframe && !isRobotsTxt) {
     // do nothing
   } else {
@@ -1933,7 +1934,7 @@ if (document.location.hostname.startsWith('music')) {
     })
   }
 
-  if (isRobotsTxt === false) {
+  if (isRobotsTxt === false && genius !== null && genius.option) {
     GM.registerMenuCommand(SCRIPT_NAME + ' - Show lyrics', () => addLyrics(true))
 
     function videoTimeUpdate (ev) {

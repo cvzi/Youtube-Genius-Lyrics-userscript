676b3be68b4d6ba002beb475148f7f798fa09453
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2023-10-08 06:25:04 +0900

    Version bump to update GeniusLyrics.js

--- YoutubeGeniusLyrics.user.js
+++ YoutubeGeniusLyrics.user.js
@@ -14,7 +14,7 @@
 // @author          cuzi
 // @icon            https://raw.githubusercontent.com/hfg-gmuend/openmoji/master/color/72x72/E044.png
 // @supportURL      https://github.com/cvzi/Youtube-Genius-Lyrics-userscript/issues
-// @version         10.9.39
+// @version         10.9.40
 // @require         https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js
 // @grant           GM.xmlHttpRequest
 // @grant           GM.setValue
